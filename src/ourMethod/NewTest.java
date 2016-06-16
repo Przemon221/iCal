047191b9d0458ad1aec0ed7116fc3b74c52c8de9
atrando <atrando.trando@gmail.com>
2016-06-17 00:06:38 +0200

    Test Version Tests in Progress

---
+++ src/ourMethod/NewTest.java
@@ -0,0 +1,24 @@
+package ourMethod;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewTest {
+
+	@Test
+	public void testGetDateStart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDateEnd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+}
